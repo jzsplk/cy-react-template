52bd4e4e8b4ddc4c2cdcbf6ae3fb7e512adac433
jzsplk <jzsplking@gmail.com>
2020-02-23 21:34:15 +0800

    ✅ update cypress test

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -2,18 +2,20 @@
 
 describe('My First Test', function() {
   it('visit shopper page', function() {
-		cy.visit('https://yzhang1205after.returnscenter.io/')
-		cy.contains('We accept returns of unused and undamaged items according to our returns policy.')
+		cy.visit('https://admine2e.returnscenter.com/')
+		cy.contains('Returns Center')
 
 		cy.get('a:contains("View Full Policy")',)
 
-		cy.get('#order_number').type('1041')
-		cy.get('#customer_email').type('jf.ke+test@aftership.com')
+		cy.get('#order_number').type('1248')
+		cy.get('#customer_email').type('y.zhang@aftership.com')
 
 		cy.get('button:contains("Find Your Order")').click()
 
 		cy.get('a:contains("Create Returns")').as('createReturnsButton')
 
 		cy.get('@createReturnsButton').click();
+
+		cy.contains('What would you like to return ?')
   })
 })
