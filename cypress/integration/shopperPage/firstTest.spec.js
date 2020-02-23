82856cee3744b6b2ac21aa538fd34772131b21a2
jzsplk <jzsplking@gmail.com>
2020-02-23 22:59:00 +0800

    ♻️update test code

--- cypress/integration/shopperPage/firstTest.spec.js
+++ cypress/integration/shopperPage/firstTest.spec.js
@@ -7,10 +7,11 @@ describe('My First Test', function() {
 
 		cy.get('a:contains("View Full Policy")',)
 
-		cy.get('#order_number').type('1248')
-		cy.get('#customer_email').type('y.zhang@aftership.com')
-
-		cy.get('button:contains("Find Your Order")').click()
+		cy.get('input').get("[name='order_number']").type('1248').should('have.value', '1248')
+		// cy.get('input[name="customer_email"]').get().type('y.zhang@aftership.com')
+		// cy.get('#customer_email').type('y.zhang@aftership.com')
+		cy.get("[name='customer_email']").type('y.zhang@aftership.com{enter}').should('have.value', 'y.zhang@aftership.com').type('{enter}')
+		// cy.get('button:contains("Find Your Order")').click()
 
 		cy.get('a:contains("Create Returns")').as('createReturnsButton')
 
