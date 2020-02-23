56b99f1f75dd2cca9f40267eb5bf85cce51c0cea
jzsplk <jzsplking@gmail.com>
2020-02-23 17:46:17 +0800

    try deploy again

---
+++ build/precache-manifest.bb1dce1fbdced54ea0f4ee534755a5f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c683239f2cbfb04b9553ce0b3234b3a0",
+    "url": "/cy-react-template/index.html"
+  },
+  {
+    "revision": "e655eea5abe6b08cc96a",
+    "url": "/cy-react-template/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/cy-react-template/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cy-react-template/static/js/2.b6fabe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e655eea5abe6b08cc96a",
+    "url": "/cy-react-template/static/js/main.cf5def9e.chunk.js"
+  },
+  {
+    "revision": "3ddc15aa34e60e85af5b",
+    "url": "/cy-react-template/static/js/runtime-main.356e8c93.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cy-react-template/static/media/logo.5d5d9eef.svg"
+  }
+]);
