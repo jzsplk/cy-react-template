daeb638199fc05d58c5ff57b0d2534f0ed650a51
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-28 05:51:15 +0000

    Deploy jzsplk/cy-react-template to github.com/jzsplk/cy-react-template.git:gh-pages

---
+++ precache-manifest.f75b2d13cab4cd6cf1bd9747b15df04a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a06e9375c4dddce536a22b0ef9f87e3",
+    "url": "/cy-react-template/index.html"
+  },
+  {
+    "revision": "5771bc773c457d7e6506",
+    "url": "/cy-react-template/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/cy-react-template/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cy-react-template/static/js/2.b6fabe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5771bc773c457d7e6506",
+    "url": "/cy-react-template/static/js/main.89003f43.chunk.js"
+  },
+  {
+    "revision": "3ddc15aa34e60e85af5b",
+    "url": "/cy-react-template/static/js/runtime-main.356e8c93.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cy-react-template/static/media/logo.5d5d9eef.svg"
+  }
+]);
