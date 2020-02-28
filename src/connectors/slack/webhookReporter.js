6af8b079312536c36bbce2e9bfc46aa3840821b6
jzsplk <jzsplking@gmail.com>
2020-02-28 11:22:39 +0800

    📦update packages and travis

--- src/connectors/slack/webhookReporter.js
+++ src/connectors/slack/webhookReporter.js
@@ -1,68 +1,64 @@
 require("dotenv").config();
 
 const fs = require("fs");
-// const SlackWebhook = require("slack-webhook");
-// const slack = new SlackWebhook(process.env.SLACK_WEBHOOK_URL_ME);
-// const slack = new SlackWebhook(process.env.SLACK_WEEBHOOK_URL_PROJ_RC);
 
-const {IncomingWebhook} = require('@slack/webhook')
-// const slack = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL_ME);
-const slack = new IncomingWebhook(process.env.SLACK_WEEBHOOK_URL_MONITOR_RETURNS_PROD, {
-  icon_emoji: ':bowtie:',
-});
+const { IncomingWebhook } = require("@slack/webhook");
+const slack = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL_ME);
+// const slack = new IncomingWebhook(process.env.SLACK_WEEBHOOK_URL_MONITOR_RETURNS_PROD, {
+//   icon_emoji: ':bowtie:',
+// });
 
-function parseReport() {
-  const report = fs.readFileSync("health-check.json", "utf-8");
-  const data = JSON.parse(report);
-
-  return data;
-  }
-
-const data = parseReport();
-
-console.log('data',data);
-
-let title;
-let result;
-
-if (typeof data !== "undefined" && data.failures.length) {
-  const { failures } = data;
-  title = `<!channel> Failing ${failures.length} ${
-    failures.length > 1 ? "tests" : "test"
-  } for admine2e.returnscenter.com`;
-
-  result = failures.map(fail => {
-    return {
-      fallback: "Test summary",
-      color: "danger",
-      text: `:x: ${fail.title} on :fire:`
-    };
-  });
-} else {
-  title = "Health check for admine2e.returnscenter.com";
-  result = data.passes.map(pass => {
-    return {
-      fallback: "Health check summary",
-      color: "good",
-      text: `:white_check_mark: ${pass.fullTitle}. `
-    };
-  });
-}
-
-// console.log(slack)
-// try {
-//   slack.send({
-//     text: title,
-//     attachments: result
-//   });
-// } catch (error) {
-//   console.error('slack error',error)
-//   throw(error);
-// }
-
-(async () => {
+const reportToSlack = async function(title, result) {
   await slack.send({
     text: title,
     attachments: result
   });
-})();
+};
+
+function parseReport() {
+  const report = fs.readFileSync(
+    "cypress/reports/mocha/mochawesome.json",
+    "utf-8"
+  );
+  const data = JSON.parse(report);
+
+  return data;
+}
+
+const data = parseReport();
+
+// for slack to use
+let title;
+let result;
+
+const { results: reportResults } = data;
+const failResults = reportResults.filter(result => {
+  return result.failures.length > 0;
+});
+
+if (typeof data !== "undefined" && failResults.length > 0) {
+  title = `<!channel> Failing ${failResults.length} ${
+    failResults.length > 1 ? "tests" : "test"
+  } for admine2e.returnscenter.com`;
+
+  result = failResults.map(fail => {
+    return {
+      fallback: "Test summary",
+      color: "danger",
+      text: `:x: ${fail.tests[0].title} on :fire:`
+    };
+  });
+
+  reportToSlack(title, result);
+} else {
+  title = "Health check for admine2e.returnscenter.com";
+  result = data.results.map(pass => {
+    return {
+      fallback: "Health check summary",
+      color: "good",
+      text: `:white_check_mark: ${pass.file}. `
+    };
+  });
+}
+
+//  reportToSlack(title, result)
--- src/job/testScheduler.js
+++ src/job/testScheduler.js
@@ -1,32 +1,15 @@
-const schedule = require('node-schedule');
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const schedule = require("node-schedule");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
-const healthScheduleJob = schedule.scheduleJob('1 15 * * * *', async function(){
-	console.log('Start health check from QA bot returnsenter');
-	try {
-		const { stdout, stderr } = await exec('yarn healthcheck');
-		console.log('stdout:', stdout);
-		stderr && console.error('stderr:', stderr);
-
-	} catch (error) {
-		console.error(error)
-		throw(error);
-	}
-	// try {
-	// 	await exec(`yarn healthcheck`, (err, sdtout, stderr) => {
-	// 		if (err) {
-	// 			console.error(err);
-	// 			return
-	// 			// exec('kill $(jobs -p) || true')
-	// 		} else {
-	// 			console.log(`stdout: ${sdtout}`)
-	// 			stderr && console.log(`stderr: ${stderr}`)
-	// 			exec('kill $(jobs -p) || true')
-	// 		}
-	// 	});
-	// } catch (error) {
-	// 	console.error(error)
-	// 	throw(error);
-	// }
+const healthScheduleJob = schedule.scheduleJob("1 * * * * *", async function() {
+  console.log("Start health check from QA bot returnsenter");
+  try {
+    const { stdout, stderr } = await exec("yarn healthcheck");
+    console.log("stdout:", stdout);
+    stderr && console.error("stderr:", stderr);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 });
